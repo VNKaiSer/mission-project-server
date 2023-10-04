cba5b0accbc716b468bf4fe8cd506e50d6041599
Võ Tấn Đạt <91039754+VNKaiSer@users.noreply.github.com>
2023-10-04 14:00:50 +0700

    initial project

---
+++ index.ts
@@ -0,0 +1,5 @@
+import express from 'express';
+const app = express();
+app.get('/', (req: any, res: any) => {
+    res.send('Hello World!');
+}); app.listen(3000, () => console.log('Server ready'));                                                           
